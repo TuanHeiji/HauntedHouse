06aae29401ca31e36fa88a8d0deede137e9cdd2f
Trịnh Tuấn Anh <ttanh311.dev@gmail.com>
2026-08-09 22:50:59 +0700

    GameEnding and Audio

--- Assets/_3DStealthGame/Scripts/Observer.cs
+++ Assets/_3DStealthGame/Scripts/Observer.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Observer : MonoBehaviour
 {
     public Transform player;
+    public GameEnding gameEnding;
     bool m_IsPlayerInRange;
+
 
     void OnTriggerEnter(Collider other)
     {
@@ -35,7 +37,7 @@ public class Observer : MonoBehaviour
             {
                 if (raycastHit.collider.transform == player)
                 {
-                    Debug.Log("Player was caught!");
+                    gameEnding.CaughtPlayer();
                 }
             }
         }
